03317c23499b3b9de94ae22589d172fe0ad84bdc
Meringue-KIm <dd8873@naver.com>
2025-01-15 20:01:42 +0900

    Settings

---
+++ src/main/java/footballProject/ground_secretary/GroundSecretaryApplication.java
@@ -0,0 +1,13 @@
+package footballProject.ground_secretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundSecretaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroundSecretaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/footballProject/ground_secretary/GroundSecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package footballProject.ground_secretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundSecretaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
